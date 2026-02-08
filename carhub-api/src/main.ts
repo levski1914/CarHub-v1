740441a60e02d45b5f02261b8dc8f6358fdeb5e2
Georgi Dimitrov <austria1233@abv.bg>
2026-02-08 18:37:11 +0200

    fix main

--- carhub-api/src/main.ts
+++ carhub-api/src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://carhub-v1.onrender.com'],
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
